b320707606d71e463cce48a7737a6ec787c33fd3
Majestyk228 <klsarah75@sfr.fr>
2022-04-18 20:20:52 +0200

    HTTP done and stats started
    All HTTP testing files are done and tested -> OK
    
    Statistics endpoints are almost done (missing only the number of user).

--- server/routes/statsRoutes.js
+++ server/routes/statsRoutes.js
@@ -3,11 +3,69 @@ const stats = require('../services/stats.js');
 const express = require('express');
 statsRouter = express.Router();
 
+/*
+	TotalUtilisateurs
+	Total contacts
+	total liste
+	dernier message envoyé (id)
+	nombre total de messages envoyés
+	nombre de messages envoyés par type
+*/
+
 // =============================================
 
-// TODO : CONFIGURE ROUTES AFTER DEFINING FUNCTION
-statsRouter.get('/', async function (req, res, next) {
-	res.send("Statistics will be added soon...");
+// TODO : Number of contacts
+statsRouter.get('/contacts', async function (_, res, next) {
+	try {
+		res.send(await stats.contactCount());
+	} catch (error) {
+		console.error('Error while counting contacts');
+		next(error);
+	}
+});
+
+
+// TODO : Number of lists
+statsRouter.get('/lists', async function (req, res, next) {
+	try {
+		res.send(await stats.listCount());
+	} catch (error) {
+		console.error('Error while counting lists');
+		next(error);
+	}
+});
+
+
+// TODO : Last message sent
+statsRouter.get('/last_message', async function (req, res, next) {
+	try {
+		res.send(await stats.lastSentMessage());
+	} catch (error) {
+		console.error("Error while getting last message's id");
+		next(error);
+	}
+});
+
+
+// TODO : Number of messages sent
+statsRouter.get('/messages', async function (req, res, next) {
+	try {
+		res.send(await stats.messageCount());
+	} catch (error) {
+		console.error('Error while counting sent messages');
+		next(error);
+	}
+});
+
+
+// TODO : Number of messages per type
+statsRouter.get('/messages_per_type', async function (req, res, next) {
+	try {
+		res.send(await stats.messageCountType());
+	} catch (error) {
+		console.error('Error while counting messages');
+		next(error);
+	}
 });
 
 // =============================================
--- server/services/stats.js
+++ server/services/stats.js
@@ -2,4 +2,55 @@ const db = require('./database.js');
 
 // TODO : DEFINE ALL STATS NEEDED
 
-module.exports = {};
+/*
+contactCount()
+listCount()
+lastSentMessage()
+messageCount()
+messageCountType()
+*/
+
+async function contactCount() {
+	//select count(*) from message;
+	const rows = await db.query("SELECT COUNT(*) as nbContacts FROM contact", "");
+	return rows;
+}
+
+
+async function listCount() {
+	//select count(*) from list;
+	const rows = await db.query("SELECT COUNT(*) as nbLists FROM list", "");
+	return rows;
+}
+
+
+async function lastSentMessage() {
+	//SELECT fields FROM table ORDER BY id DESC LIMIT 1;
+	//SELECT id FROM message WHERE id_state = 1 ORDER BY id DESC LIMIT 1
+	const rows = await db.query("SELECT id as id FROM message WHERE id_state = 1 ORDER BY id DESC LIMIT 1", "");
+	return rows;
+}
+
+
+async function messageCount() {
+	//select count(*) from message WHERE id_state = 1;
+	const rows = await db.query("SELECT COUNT(*) as nbSentMessages FROM message WHERE id_state=1", "");
+	return rows;
+}
+
+
+async function messageCountType() {
+	//select count(*) from message GROUP BY id_state;
+	const rows = await db.query("SELECT COUNT(*) FROM message GROUP BY id_state;", "");
+	return rows;
+}
+
+
+
+module.exports = {
+	contactCount,
+	listCount,
+	lastSentMessage,
+	messageCount,
+	messageCountType
+};
